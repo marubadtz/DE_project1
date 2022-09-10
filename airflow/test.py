ac8df6dce9a6dc008e0db07c76803992290b92b8
marubadtz <heellooooo@gmail.com>
2022-09-10 16:48:13 +0200

    update file

--- airflow/test.py
+++ airflow/test.py
@@ -1,4 +1,16 @@
 from ibm_botocore.client import Config
 import ibm_boto3
 
-from credentials import credentials
+from airflow.credentials import credentials
+
+credentials['IBM_API_KEY_ID']
+
+from ibm_botocore.client import Config
+import ibm_boto3
+
+cos = ibm_boto3.client(service_name='s3',
+    ibm_api_key_id=credentials['IBM_API_KEY_ID'],
+    ibm_service_instance_id=credentials['IAM_SERVICE_ID'],
+    ibm_auth_endpoint=credentials['IBM_AUTH_ENDPOINT'],
+    config=Config(signature_version='oauth'),
+    endpoint_url=credentials['ENDPOINT'])
